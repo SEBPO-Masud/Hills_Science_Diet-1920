ca9929a07c0a60e40e1b9445c7ff6959b9bfcf85
SEBPO-MasudurRahman <115811230+SEBPO-MasudurRahman@users.noreply.github.com>
2023-12-25 21:27:57 +0600

    Add files via upload

--- assets/Product_Carousel.js
+++ assets/Product_Carousel.js
@@ -1,6 +1,5 @@
-// Species 
+// Species
 function filterSpeciesTab(species) {
-
   // Hide all elements with class="citrusHills__tabcontent_wrapper" by default */
   var i, citrusHills__tabcontent_wrapper, citrusHills__speciesTablinks;
   citrusHills__tabcontent_wrapper = document.getElementsByClassName(
@@ -12,14 +11,12 @@ function filterSpeciesTab(species) {
 
   // Show the specific tab content
   document.getElementById(species).style.display = "block";
-  if (species === 'citrusHills__species-dog') {
+  if (species === "citrusHills__species-dog") {
     filterDogProducts("citrusHills-puppy_Dog-product");
-  } 
-  else {
+  } else {
     filterProducts("citrusHills-kitten_Cat-product");
   }
 }
-
 
 document.getElementById("defaultOpen").click();
 
@@ -45,14 +42,9 @@ function addActiveClass(element, activeClass) {
   }
 } //for active tab switcher
 
-
-
-
 /**----------------------
-   * product scroller js ---
+   * product scroller js Dog ---
    --------------------------*/
-// tab Carousel js
-// initialize product scroller Dog
 advanceArrowsDog("citrusHills-puppy_Dog-product");
 function advanceArrowsDog(idx) {
   var splideElement = "#" + idx;
@@ -104,72 +96,28 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 // for dog filter function
 function filterDogProducts(c) {
-  var x, i;
+  var x, y, i, j;
   x = document.getElementsByClassName("citrusHills__dogProduct");
+  
+
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
     RemoveClass(x[i], "citrusDogTabShow");
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], " citrusDogTabShow");
   }
+  // for active tab class
+  y = document.getElementsByClassName("citrusHills__dog_tab_item");
+  for (j = 0; j < y.length; j++) {
+    RemoveClass(y[j], "citrusHills__active_tab_dog");
+    if (y[j].className.indexOf(c) > -1)
+      AddClass(y[j], " citrusHills__active_tab_dog");
+  }
   advanceArrowsDog(c);
 }
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-document.addEventListener("DOMContentLoaded", function () {
-  // for dog active class filter
-  var dogBtnContainer = document.getElementById("citrusHillsTabIdDog");
-  var dogBtns = dogBtnContainer.getElementsByClassName(
-    "citrusHills__dog_tab_item"
-  );
-  addActiveClass(dogBtns, "citrusHills__active_tab_dog");
-});
-function addActiveClass(element, activeClass) {
-  for (var i = 0; i < element.length; i++) {
-    element[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName(activeClass);
-      current[0].className = current[0].className.replace(
-        " " + activeClass,
-        ""
-      );
-      this.className += " " + activeClass;
-    });
-  }
-} //for Dog best seller tab switcher
-
-
-
-
-
-
-
 
 /**----------------------
-   * product scroller js ---
+   * product scroller js Cat ---
    --------------------------*/
-// tab Carousel js
-// initialize product scroller Cat
 advanceArrows("citrusHills-kitten_Cat-product");
 function advanceArrows(idx) {
   var splideElement = "#" + idx;
@@ -219,14 +167,23 @@ function advanceArrows(idx) {
 document.addEventListener("DOMContentLoaded", function () {
   filterProducts("citrusHills-kitten_Cat-product");
 });
-// for dog filter function
+// for cat filter function
 function filterProducts(c) {
-  var x, i;
+  var x, y, i, j;
   x = document.getElementsByClassName("citrusHills__Product");
+  
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
     RemoveClass(x[i], "citrusTabShow");
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], " citrusTabShow");
+  }
+
+  // for active tab class
+  y = document.getElementsByClassName("citrusHills__tab_item");
+  for (j = 0; j < y.length; j++) {
+    RemoveClass(y[j], "citrusHills__active_tab");
+    if (y[j].className.indexOf(c) > -1)
+      AddClass(y[j], " citrusHills__active_tab");
   }
   advanceArrows(c);
 }
@@ -252,23 +209,3 @@ function RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
-
-// Add active class to the current button (highlight it)
-document.addEventListener("DOMContentLoaded", function () {
-  // for dog active class filter
-  var dogBtnContainer = document.getElementById("citrusHillsTabId");
-  var dogBtns = dogBtnContainer.getElementsByClassName("citrusHills__tab_item");
-  addActiveClass(dogBtns, "citrusHills__active_tab");
-});
-function addActiveClass(element, activeClass) {
-  for (var i = 0; i < element.length; i++) {
-    element[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName(activeClass);
-      current[0].className = current[0].className.replace(
-        " " + activeClass,
-        ""
-      );
-      this.className += " " + activeClass;
-    });
-  }
-} //for Dog best seller tab switcher
